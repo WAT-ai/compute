e4d5bc9b7ad98ba38fbaf22c2470e4199439828c
Areel Khan <areel15511@gmail.com>
2024-02-12 23:25:39 -0500

    added new user files

---
+++ new_user_files/test.py
@@ -0,0 +1,14 @@
+print("Running test job")
+
+import torch
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-large")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-large", device_map="auto", torch_dtype=torch.float16)
+
+input_text = "translate English to German: How old are you?"
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0]))
