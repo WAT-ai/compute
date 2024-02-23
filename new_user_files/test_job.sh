456d1e9d48245f6b0dfa2deb3f6211f0de8cdc59
Areel Ullah Khan <76454082+AreelKhan@users.noreply.github.com>
2024-02-23 16:22:54 -0500

    Update test_job.sh

--- new_user_files/test_job.sh
+++ new_user_files/test_job.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes - you only have 1 node, so you only have 1 task. Leave this.
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks) - play with this number if you are using a lot of CPU, but most people are using these machines for GPU only
 #SBATCH --gres=gpu:1 #most machines have a single GPU, so leave this as-is. If you are on a dual GPU partition, this can be changed to --gres=gpu:2 to use both
-#SBATCH --mem-per-cpu=12G         # memory per cpu-core - unless you're doing something obscene the default here is fine. This is RAM, not VRAM, so it's like storage for your dataset
-#SBATCH --time=00:10:00          # total run time limit - You can increase this however you wish, depending on your job's needs. However, it is a good idea to keep it to what you need, in case your job goes off
+#SBATCH --mem-per-cpu=12G        # memory per cpu-core - unless you're doing something obscene the default here is fine. This is RAM, not VRAM, so it's like storage for your dataset
+#SBATCH --time=00:10:00          # total run time limit (HH:MM:SS)
 #SBATCH --output=/slurm_nfs/test_user/job_output/%j.out
 
 #If you are using your own custom venv, replace mine with yours. Otherwise, stick to this default. It has torch, transformers, accelerate and a bunch of others. I'm happy to add more common libraries
